e3d4d0608f05632e58663f5fb8c35cae25ebd017
Talles <airan.talles@gmail.com>
2018-04-11 17:28:40 -0300

    Initial Commit
    Initial changes

--- wp-content/themes/constrular/README.md
+++
@@ -1,2 +0,0 @@
-Bfriend Codes Framework
-========================
--- wp-content/themes/constrular/partials/_assinatura-bfriend.php
+++
@@ -1,5 +0,0 @@
-<div id="bfriend">
-  <a href="http://bfriend.com.br" target="_blank">
-		<img id="logo" src="http://bfriend.com.br/assinaturas/bfriend.png" class="img-fluid">
-  </a>
-</div>
